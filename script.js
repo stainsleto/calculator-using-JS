59b8e2b201168b1bc2c81921e0a98313259667ac
Stains <stainsmault@gmail.com>
2022-08-20 17:03:50 +0530

    calculator using js
    done using js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function printSymbol(e){
-    let result = document.getElementById("result");
-    let obj = e.target;
-    result.value = result.value + obj.textContent;
-}
+
 
 // clicked object is 0 or not 
 
-function printDigit(e){
+function printSymbol(e){
     let result = document.getElementById("result");
     let obj = e.target;
     if (result.value == 0){
@@ -18,49 +14,18 @@ function printDigit(e){
     
 }
 
-
-
-let result = document.getElementById("result");
-let num1 = document.getElementById("num1");
-let num2 = document.getElementById("num2");
-let num3 = document.getElementById("num3");
-let num4 = document.getElementById("num4");
-let num5 = document.getElementById("num5");
-let num6 = document.getElementById("num6");
-let num7 = document.getElementById("num7");
-let num8 = document.getElementById("num8");
-let num9 = document.getElementById("num9");
-let num0 = document.getElementById("num0");
-
 //signs
-let subtraction = document.getElementById("subtraction");
-let addition = document.getElementById("addition");
-let multiplication = document.getElementById("multiplication");
-let division = document.getElementById("division");
-let decimal = document.getElementById("decimal");
+let result = document.getElementById("result");
 let equal = document.getElementById("equal");
 let clear = document.getElementById("clear");
-let rightp = document.getElementById("rightp");
-let leftp = document.getElementById("leftp");
-//numbers
-num1.addEventListener("click", printDigit);
-num2.addEventListener("click", printDigit);
-num3.addEventListener("click", printDigit);
-num4.addEventListener("click", printDigit);
-num5.addEventListener("click", printDigit);
-num6.addEventListener("click", printDigit);
-num7.addEventListener("click", printDigit);
-num8.addEventListener("click", printDigit);
-num9.addEventListener("click", printDigit);
-num0.addEventListener("click", printDigit);
-// signs
-subtraction.addEventListener("click",printSymbol);
-addition.addEventListener("click",printSymbol);
-multiplication.addEventListener("click",printSymbol);
-division.addEventListener("click",printSymbol);
-decimal.addEventListener("click",printSymbol);
-leftp.addEventListener("click", printSymbol);
-rightp.addEventListener("click",printSymbol);
+
+//sign and number execution using loop on click
+
+let symbols = document.getElementsByClassName("symbol");
+
+for (let i=0;i<symbols.length;i++){
+    symbols[i].addEventListener("click",printSymbol);
+}
 
 // evaluation for equal sign 
 
